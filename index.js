56d29242c2ce1260e962e1483fecc4a715a59ae5
ApenasGabs <Gabers357@gmail.com>
2021-10-13 21:15:18 -0300

    banco de dados configurado

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const customExpress = require("./config/customExpress");
+const customExpress = require("./config/customExpress")
+const conexao = require("./infra/conexao")
 
+conexao.connect((error) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('banco de dados conectado🙌');
+        app.listen(3000,
+            () => console.log("servidor rodando 👁️ 👄👁️"));
+    }
+});
 const app = customExpress();
-
-app.listen(3000, () => console.log("servidor rodando 👁️ 👄👁️"));
