f8c30dd31df5a13294588786a9d59ed6925c5ec8
ApenasGabs <Gabers357@gmail.com>
2021-10-13 22:09:33 -0300

    criada funcao de criar tabelas sql

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const customExpress = require("./config/customExpress")
 const conexao = require("./infra/conexao")
+const Table = require('./infra/tables')
+const customExpress = require("./config/customExpress")
 
 conexao.connect((error) => {
     if (error) {
         console.log(error);
     } else {
+        const app = customExpress();
         console.log('banco de dados conectado🙌');
+        Table.init(conexao)
         app.listen(3000,
             () => console.log("servidor rodando 👁️ 👄👁️"));
     }
 });
-const app = customExpress();
--- infra/conexao.js
+++ infra/conexao.js
@@ -4,7 +4,7 @@ const conexao = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '!2gA#216Rasd',
-    databese: 'agenda-petshop'
+    database: 'agenda-petshop'
 })
 
 module.exports = conexao
